3d6f18f0df8dbd07bf40869b0b76151904cdf41e
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-04-23 15:02:27 +0200

    gdl

--- models/ff.py
+++ models/ff.py
@@ -1,14 +1,15 @@
+import time
+
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torch.utils.data.dataloader as dl
+import torch.utils.data.dataloader as loader
 from sklearn.datasets import load_breast_cancer
-from torch import tensor
 
 
-class MLP(nn.Module):
+class NeuralNetwork(nn.Module):
     def __init__(self, layers_sizes: list[int]) -> None:
         self.layers = [
             nn.Linear(size, layers_sizes[i + 1]) for i, size in enumerate(layers_sizes)
@@ -22,7 +23,12 @@ class MLP(nn.Module):
 
 
 if __name__ == "__main__":
-    data = load_breast_cancer()
+    X, y = load_breast_cancer(return_X_y=True)
 
-    X = torch.tensor(data.data)
-    y = torch.tensor(data.target)
+    X = torch.tensor(X)
+    y = torch.tensor(y)
+
+    network = NeuralNetwork([32, 32, 32])
+    network.fit(X, y)
+
+    y_pred = network.predict(X)
